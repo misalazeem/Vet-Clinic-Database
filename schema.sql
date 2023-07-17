50687edc08ef4a44f2cb36f0a2a608be7ee140dd
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-07-17 17:41:51 +0500

    adding queries

--- schema.sql
+++ schema.sql
@@ -69,6 +69,9 @@ CREATE TABLE visits
   visit_date DATE
 );
 
-ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+ALTER TABLE owners ADD COLUMN email VARCHAR
+(120);
 
 CREATE INDEX idx_visits_animal_id ON visits (animal_id);
+CREATE INDEX idx_visits_vet_id ON visits (vet_id);
+CREATE INDEX idx_owners_email ON owners (email);
