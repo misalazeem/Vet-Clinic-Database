87e87e780e1bbd9a1de939d724d951afa117469f
Luis Zubia <soyzubia@gmail.com>
2023-07-18 06:08:58 -0700

    VET CLINIC DB: Add last tables and foreign keys indexes

--- database_from_schema_diagram.sql
+++ database_from_schema_diagram.sql
@@ -34,3 +34,34 @@ if not exists public.invoices
 (medical_history_id) REFERENCES medical_histories
 (id)
 );
+
+CREATE TABLE if not exists public.treatments (
+  id integer NOT NULL PRIMARY KEY,
+  type VARCHAR(100),
+  name VARCHAR(100)
+);
+
+CREATE TABLE if not exists public.treatments_histories (
+  medical_history_id integer NOT NULL,
+  treatment_id integer NOT NULL,
+  FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+  FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+);
+
+CREATE TABLE public.invoice_items (
+  id integer NOT NULL PRIMARY KEY,
+  unit_price DECIMAL NOT NULL,
+  quantity integer NOT NULL,
+  total_price DECIMAL NOT NULL,
+  invoice_id integer NOT NULL,
+  treatment_id integer NOT NULL,
+  FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+  FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+);
+
+CREATE INDEX invoice_items_invoice_id ON invoice_items(invoice_id);
+CREATE INDEX invoice_items_treatment_id ON invoice_items(treatment_id);
+CREATE INDEX treatments_histories_medical_history_id ON treatments_histories(medical_history_id);
+CREATE INDEX treatments_histories_treatment_id ON treatments_histories(treatment_id);
+CREATE INDEX invoices_medical_history_id ON invoices(medical_history_id);
+CREATE INDEX medical_histories_patient_id ON medical_histories(patient_id);
